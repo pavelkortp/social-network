364c5e02bee681ee76941e9c6636b50966762e06
Pavlo Zhurbytskyi <pavelkortp@gmail.com>
2024-03-03 22:57:25 +0200

    init commit

---
+++ app/src/main/java/com/pavelkortp/socialnetwork/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pavelkortp.socialnetwork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
