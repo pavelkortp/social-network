d9ddf8dbcc068bb0d651b1c1650e475287dc3a0b
Pavlo Zhurbytskyi <pavelkortp@gmail.com>
2024-03-11 00:47:31 +0200

    update buttons design

--- app/src/main/java/com/pavelkortp/socialnetwork/MainActivity.kt
+++ app/src/main/java/com/pavelkortp/socialnetwork/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.pavelkortp.socialnetwork
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.pavelkortp.socialnetwork.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.LogOut.setOnClickListener {
+            closeApp()
+        }
+    }
+
+    private fun closeApp(){
+        binding.Name.setTextColor(Random.nextInt())
     }
 }
