0695dc778e8b371beacfa7501263e1347fdc3700
Pavlo Zhurbytskyi <pavelkortp@gmail.com>
2024-03-15 22:37:48 +0200

    adds circled images

--- app/src/main/java/com/pavelkortp/socialnetwork/MainActivity.kt
+++ app/src/main/java/com/pavelkortp/socialnetwork/MainActivity.kt
@@ -1,26 +1,48 @@
 package com.pavelkortp.socialnetwork
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.pavelkortp.socialnetwork.databinding.ActivityMainBinding
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.LogOut.setOnClickListener {
-            closeApp()
-        }
+//        binding.SearchBTN.setOnClickListener {
+//            searchImg()
+//        }
+//
+//        binding.SearchField.setOnEditorActionListener { _, actionId, _ ->
+//            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+//                return@setOnEditorActionListener searchImg()
+//            }
+//            return@setOnEditorActionListener false
+//        }
+
     }
 
-    private fun closeApp(){
-        binding.Name.setTextColor(Random.nextInt())
-    }
+//    private fun searchImg(): Boolean {
+//        val keyword: String = binding.SearchField.text.toString()
+//
+//        if (keyword.isBlank()) {
+//            binding.SearchField.error = "Keyword is empty"
+//            return true
+//        }
+//
+//        val encodedKeyword: String = URLEncoder.encode(keyword, StandardCharsets.UTF_8.name())
+//
+//        Glide.with(this)
+//            .load("https://source.unsplash.com/random/300x200?$encodedKeyword")
+//            .skipMemoryCache(true)
+//            .diskCacheStrategy(DiskCacheStrategy.NONE)
+//            .placeholder(R.drawable.bob)
+//            .into(binding.Pic)
+//        return false
+//    }
+
+
 }
